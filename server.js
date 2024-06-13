6ebc045e2201b3f90754c67ddadbc4b97d17a309
Matveyk9 <153567537+Matveyk9@users.noreply.github.com>
2024-06-14 00:21:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const keywords = {
 		"https://ru.wikipedia.org/wiki/Заглавная_страница",
 		"https://ru.wikipedia.org/wiki/Википедия:Форум",
 	],
-	Book: [
-		"https://www.ozon.ru/category/knigi-16500/?category_was_predicted=true&deny_category_prediction=true&from_global=true&text=%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0",
+	Compound: [
+		"https://www.agrosadovod.ru/catalog/kombikorma/",
 	]
 };
 
