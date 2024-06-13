90a24cdfc77215f101c7c453fbdf86513595e08b
Matveyk9 <153567537+Matveyk9@users.noreply.github.com>
2024-06-14 01:13:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const WebSocket = require("ws");
 const app = express();
 const port = 3000;
 
-// Хранилище ключевых слов и соответствующих URL
+
 const keywords = {
 	college: [
 		"https://ithub.ru/",
@@ -22,47 +22,15 @@ const keywords = {
 };
 
 app.use(express.json());
-// Использование cors для разрешения запросов из других источников
+
 app.use(cors());
 
-// Эндпоинт для получения списка URL по ключевому слову
 app.post("/get-urls", (req, res) => {
 	const keyword = req.body.keyword;
 	const urls = keywords[keyword] || [];
 	res.json({ urls });
 });
 
-// Эндпоинт для скачивания контента по URL
-
-// app.post("/download", async (req, res) => {
-// 	const url = req.body.url;
-// 	const totalLength = (await axios.head(url)).headers["content-length"];
-
-// 	axios({
-// 		method: "get",
-// 		url: url,
-// 		responseType: "stream",
-// 	})
-// 		.then((response) => {
-// 			let downloadedLength = 0;
-// 			response.data.on("data", (chunk) => {
-// 				downloadedLength += chunk.length;
-// 				const progress = Math.round(
-// 					(downloadedLength / totalLength) * 100
-// 				);
-// 				res.write(
-// 					JSON.stringify({ progress, downloadedLength, totalLength })
-// 				);
-// 			});
-
-// 			response.data.on("end", () => {
-// 				res.end();
-// 			});
-// 		})
-// 		.catch((error) => {
-// 			res.status(500).json({ error: "Failed to download content" });
-// 		});
-// });
 const server = app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
 });
